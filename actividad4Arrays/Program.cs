c8e80d40adc026568c4734b9302602e0b9d89291
AdrianWanBer <146719913+AdrianWanBer@users.noreply.github.com>
2023-12-18 14:19:01 +0100

    Update Program.cs

--- actividad4Arrays/Program.cs
+++ actividad4Arrays/Program.cs
@@ -9,18 +9,18 @@
             Console.WriteLine("¿Que tamaño quieres que tenga el vector? :");
             int tamañoVector = Convert.ToInt32(Console.ReadLine());
 
-            int[] VectorNumeros = new int[tamañoVector];
+            int[] vectorNumeros = new int[tamañoVector];
 
             for (int i = 0; i < tamañoVector; i++)
             {
                 int valorRandom = r.Next(1, 100);
-                VectorNumeros[i] = valorRandom;
+                vectorNumeros[i] = valorRandom;
 
             }
             Array.Sort(VectorNumeros);
             for (int j = 0; j < tamañoVector; j++)
             {
-                Console.WriteLine(VectorNumeros[j]);
+                Console.WriteLine(vectorNumeros[j]);
             }
 
         }
