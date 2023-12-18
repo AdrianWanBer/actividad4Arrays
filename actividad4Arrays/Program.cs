bb1366ab84746a0f94a2a622a0761c98b92fbde4
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2023-12-18 14:16:08 +0100

    Agregar archivos de proyecto.

---
+++ actividad4Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace actividad4Arrays
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+
+            Random r = new Random();
+            Console.WriteLine("¿Que tamaño quieres que tenga el vector? :");
+            int tamañoVector = Convert.ToInt32(Console.ReadLine());
+
+            int[] VectorNumeros = new int[tamañoVector];
+
+            for (int i = 0; i < tamañoVector; i++)
+            {
+                int valorRandom = r.Next(1, 100);
+                VectorNumeros[i] = valorRandom;
+
+            }
+            Array.Sort(VectorNumeros);
+            for (int j = 0; j < tamañoVector; j++)
+            {
+                Console.WriteLine(VectorNumeros[j]);
+            }
+
+        }
+    }
+}
